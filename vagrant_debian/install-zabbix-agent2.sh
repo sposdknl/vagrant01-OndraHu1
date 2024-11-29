594e5bd013aba74a6fc5d9f93996507ef203bc25
Hušek Ondřej <husek.ondra1@seznam.cz>
2024-11-29 09:30:30 +0100

    Přepsání scriptu, apt-get -> apt

--- vagrant_debian/install-zabbix-agent2.sh
+++ vagrant_debian/install-zabbix-agent2.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # Instalace nezbytných balíčků
-sudo apt-get update -y
-sudo apt-get install -y wget gnupg2 net-tools
+sudo apt update -y
+sudo apt install -y wget gnupg2 net-tools
 
 # Přidání Zabbix repozitáře
 wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_latest+debian11_all.deb
 sudo dpkg -i zabbix-release_latest+debian11_all.deb
 
 # Aktualizace repository
-sudo apt-get update -y
+sudo apt update -y
 
 # Instalace Zabbix Agent2 a jeho pluginů
-sudo apt-get install -y zabbix-agent2 zabbix-agent2-plugin-*
+sudo apt install -y zabbix-agent2 zabbix-agent2-plugin-*
 
 # Povolení služby Zabbix Agent2
 sudo systemctl enable zabbix-agent2
@@ -23,4 +23,3 @@ sudo systemctl restart zabbix-agent2
 # Kontrola stavu služby Zabbix Agent2
 sudo systemctl status zabbix-agent2 --no-pager
 
-# EOF
