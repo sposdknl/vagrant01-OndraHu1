f45e4f0c8b146f5540832004197626bd16279a76
Hušek Ondřej <husek.ondra1@seznam.cz>
2024-12-06 08:18:15 +0100

    changed install zabbix

--- Ubuntu/install-zabbix-agent2.sh
+++ Ubuntu/install-zabbix-agent2.sh
@@ -1,24 +1,18 @@
 #!/usr/bin/env bash
 
+sudo -s
 # Instalace balicku net-tools
-sudo apt-get install -y net-tools
+apt install -y net-tools
 
 # Stažení balíčku pro instalaci zabbix repo
-sudo wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest+ubuntu22.04_all.deb
+wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_6.0+ubuntu24.04_all.deb
+dpkg -i zabbix-release_latest_6.0+ubuntu24.04_all.deb
+apt update
 
 # Instalace meta balíčku
-sudo dpkg -i zabbix-release_latest+ubuntu22.04_all.deb
+apt install zabbix-agent2 zabbix-agent2-plugin-*
 
-# Aktualizace repository
-sudo apt-get update
-
-# Instalace meta balíčku
-sudo apt-get install -y zabbix-agent2 zabbix-agent2-plugin-*
-
-# Povoleni sluzby zabbix-agent2
-sudo systemctl enable zabbix-agent2
-
-# Restart sluzby zabbix-agent2
-sudo systemctl restart zabbix-agent2
+systemctl enable zabbix-agent2
+systemctl restart zabbix-agent2
 
 # EOF
